748807dcc94883a949587449e3ea424c5f6fe334
allanhue <allanmwangi329@gmail.com>
2025-08-03 22:23:02 +0300

    small feaature

--- servefront/src/superbaseClient.js
+++ servefront/src/superbaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://itsxxuhjfmizksjeuuot.supabase.co'
-const supabaseKey = process.env.SUPABASE_ANON_KEY // or service_role for server-side
+const supabaseKey = process.env.SUPABASE_ANON_KEY 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
