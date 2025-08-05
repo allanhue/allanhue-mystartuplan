13b282ff462416efda5943a834487baaa3897c1b
allanhue <allanmwangi329@gmail.com>
2025-08-05 09:15:13 +0300

    added themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
@@ -9,12 +8,10 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <AuthProvider>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
